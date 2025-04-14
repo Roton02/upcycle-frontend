5555402d4be50d50c388d791854e91fe8d4d449b
mahmud444t <ferdausemahmud@gmail.com>
2025-04-14 22:28:06 +0600

    added trending products data

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -4,6 +4,7 @@ import Banner from "@/components/modules/home/Banner";
 import NewArrivals from "@/components/modules/home/NewArrivals";
 import NewsletterSection from "@/components/modules/home/NewsLetterSection";
 import SupportFeatures from "@/components/modules/home/SupportFeatures";
+import TrendingProducts from "@/components/modules/home/TrendingProducts";
 import { useUser } from "@/context/UserContext";
 
 const HomePage = () => {
@@ -14,6 +15,7 @@ const HomePage = () => {
       <Banner />
       <SupportFeatures />
       <NewArrivals />
+      <TrendingProducts />
       <NewsletterSection />
     </div>
   );
