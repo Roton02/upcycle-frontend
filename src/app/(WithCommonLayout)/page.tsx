f4b7bf9c6777a3894377ff6a180447d05d40c899
mahmud444t <ferdausemahmud@gmail.com>
2025-04-14 22:42:08 +0600

    added special offers section

--- src/app/(WithCommonLayout)/page.tsx
+++ src/app/(WithCommonLayout)/page.tsx
@@ -3,6 +3,7 @@
 import Banner from "@/components/modules/home/Banner";
 import NewArrivals from "@/components/modules/home/NewArrivals";
 import NewsletterSection from "@/components/modules/home/NewsLetterSection";
+import SpecialOffers from "@/components/modules/home/SpecialOffers/SpecialOffers";
 import SupportFeatures from "@/components/modules/home/SupportFeatures";
 import TrendingProducts from "@/components/modules/home/TrendingProducts";
 import { useUser } from "@/context/UserContext";
@@ -16,6 +17,7 @@ const HomePage = () => {
       <SupportFeatures />
       <NewArrivals />
       <TrendingProducts />
+      <SpecialOffers />
       <NewsletterSection />
     </div>
   );
