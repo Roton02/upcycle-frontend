0cf3fb34abaf392158306b6139b52aa9e7343c46
mahmud444t <ferdausemahmud@gmail.com>
2025-04-25 23:57:52 +0600

    deploy issue 3

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,9 +1,12 @@
+import React, { Suspense } from "react";
 import RegisterForm from "@/components/modules/auth/register/RegisterForm";
 
 const RegisterPage = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <RegisterForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <RegisterForm />
+      </Suspense>
     </div>
   );
 };
