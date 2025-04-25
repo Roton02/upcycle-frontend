b2305f11cfed5273883e43002630aca67e666e3c
mahmud444t <ferdausemahmud@gmail.com>
2025-04-25 23:38:17 +0600

    deploy issue 2

--- src/app/(WithCommonLayout)/products/[id]/page.tsx
+++ src/app/(WithCommonLayout)/products/[id]/page.tsx
@@ -1,13 +1,13 @@
 import { getSingleProduct } from "@/services/Products";
 import ProductDetailsClient from "@/components/modules/product/ProductDetailsClient";
 
-interface ProductPageProps {
-  params: {
-    id: string;
-  };
-}
+export default async function ProductPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
 
-export default async function ProductPage({ params }: ProductPageProps) {
-  const { data } = (await getSingleProduct(params.id)) ?? { data: {} };
+  const { data } = (await getSingleProduct(id)) ?? { data: {} };
   return <ProductDetailsClient data={data} />;
 }
